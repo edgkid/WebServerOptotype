72d5f3bb9ccf2793b52139008e220281a0d68e37
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-09-03 06:03:26 -0430

    solventado probelma de conexion

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
     require_once 'UserApi.php';
-    require_once 'PatientApi.php';
     
     $userApi = new UserApi();
     $userApi->API();
-    
-    $patientApi = new PatientApi();
-    $patientApi->API();
-    
     
 ?>
