3490b61b8fa26d36dbbc0ccee85a76069078fe60
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-10-18 16:20:22 -0400

    clase con metodo para consultar optotypos

---
+++ OptotypeDB.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of OptotypeDB
+ *
+ * @author Edgar
+ */
+class OptotypeDB extends PgDataBase{
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    /**
+     * obtiene todos los registros de la tabla "optotype"
+     * @return Array array con los registros obtenidos de la base de datos
+     */
+    public function getOptotypes (){
+        
+        $data = array();
+        $query = "SELECT IdOptotype, OptotypeCode, OptotypeName "
+                . "FROM Optotype";
+        $users = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+       
+        while ($line = pg_fetch_array($users, null, PGSQL_ASSOC)) {
+            $data[] = $line;
+        }
+     
+        return $data; 
+    }
+    
+}
