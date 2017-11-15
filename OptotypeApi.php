e2d8dd3f264c15747787f7743ca2edab28a26d51
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-11-15 11:28:46 -0400

    commit sobre Optotype API para continuar desarrollo

---
+++ OptotypeApi.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once 'OptotypeDb.php';
+/**
+ * Description of OptotypeApi
+ *
+ * @author Edgar
+ */
+class OptotypeApi {
+    //put your code here
+    
+    Public function API(){
+        
+        header('Content-Type: application/JSON');                
+        $method = $_SERVER['REQUEST_METHOD'];
+        switch ($method) {
+        case 'GET'://consulta
+            $this->getUsers();
+            break;     
+        case 'POST'://inserta
+            echo'post';
+            break;                
+        case 'PUT'://actualiza
+            echo'put';
+            break;      
+        case 'DELETE'://elimina
+            echo'delete';
+            break;
+        default://metodo NO soportado
+            echo 'METODO NO SOPORTADO';
+            break;
+        }
+    }
+    
+     /**
+    * función que segun el valor de "action" e "id":
+    *  - mostrara una array con todos los registros de personas
+    *  - mostrara un solo registro 
+    *  - mostrara un array vacio
+    */
+    function getUsers(){
+       if ($_GET['action'] == 'optotypes'){
+           $db = new OptotypeDB();
+           if(isset($_GET['id'])){
+               //$response = $db->getUserById($_GET['id']);
+               echo json_encode($response,JSON_PRETTY_PRINT);
+           }elseif (isset($_GET['userName']) && isset($_GET['userPassword'])) {
+                //$response = $db->getUserByLogin($_GET['userName'],$_GET['userPassword']);
+                echo json_encode($response,JSON_PRETTY_PRINT);
+            }else{
+               $response = $db->getOptotypes();
+               echo json_encode($response,JSON_PRETTY_PRINT);
+           }
+       }else{
+           $this->response(400);
+       }  
+    }
+    
+     /**
+    * Respuesta al cliente
+    * @param int $code Codigo de respuesta HTTP
+    * @param String $status indica el estado de la respuesta puede ser "success" o "error"
+    * @param String $message Descripcion de lo ocurrido
+    */
+    function response($code=200, $status="", $message="") {
+       http_response_code($code);
+       if( !empty($status) && !empty($message) ){
+           $response = array("status" => $status ,"message"=>$message);  
+           echo json_encode($response,JSON_PRETTY_PRINT);    
+       }   
+    }  
+    
+}
