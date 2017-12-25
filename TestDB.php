ff7c890194061dbffc15fe97bdd389be839a4757
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-12-25 17:13:03 -0400

    puedo enviar desde insomnia los datos de interacción y guardarlos en TEST y OPtotypeTEST con data consistente

--- TestDB.php
+++ TestDB.php
@@ -12,7 +12,8 @@ class TestDB extends PgDataBase{
         
         $query="";
         $idPatient = "";
-
+        $idTest = "";
+        
         foreach ($obj as $value){
          
             $idPatient = $this->getFkAppointmentForTest($value->idPatient);
@@ -20,11 +21,56 @@ class TestDB extends PgDataBase{
             $query = $query."'".$value->testCode."','".$value->eye."',".$idPatient.")";
             
             $result = pg_query($query);
-                if($result)
-                    echo 'exito al guardar Test'.$value->testCode."\n";
+                if($result){
+                    
+                    $this->sendCommit();
+                    $idTest = $this->getIdTestOptotype();
+                    echo 'exito al guardar Test '.$value->testCode." ".$idTest."\n";
+                    $this->saveOptotpeTest($value->idOptotype, $idTest);
+                }
                 else 
                     echo 'fallo al guardar Test'.$value->testCode."\n";
         }
+    }
+    
+    private function saveOptotpeTest($idOptotype, $idTest){
+        
+        $query = "INSERT INTO OPTOTYPE_TEST (fk_idoptotype, fk_idtest) VALUES (";
+        $query = $query.$idOptotype.",".$idTest.")";
+        
+        $result = pg_query($query);
+                if($result)
+                    echo 'exito al guardar optotipo'.$idOptotype."\n";
+                else 
+                    echo 'fallo al guardar optotipo'.$idOptotype."\n";
+       
+    }
+    
+    private function getIdTestOptotype (){
+        
+        $value= "";
+        $query="SELECT MAX(idTest) FROM TEST";
+        
+        $result = pg_query($query);
+        
+        if ($row = pg_fetch_row($result)) {
+           
+            $value = $row[0]; 
+        }
+        
+        if ($value == null || $value == 0)
+            $value = 1;
+   
+        return $value;
+    }
+    
+    private function sendCommit (){
+        
+        $query = "COMMIT; ";
+        $result = pg_query($query);
+        if($result)
+            echo 'commit\n';
+        
     }
     
     private function getFkAppointmentForTest($idPatient){
