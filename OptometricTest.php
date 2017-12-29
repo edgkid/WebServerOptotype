2e1f96780b822d5ea3f115a098b7f50cc9b5911d
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-12-29 13:35:33 -0430

    creo clase mediante la cual se comenzara a crear la carta con sus respectivos optotipos

---
+++ OptometricTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class OptometricTest extends NewImage {
+    
+    private $testCode = "";
+    private $patient;
+    private $interaction = array();
+    
+    function __construct($testCode) {
+        $this->testCode = $testCode;
+    }
+    
+    /*Metodo para obtener resutados de interacción*/
+    function findInteractionData (){
+        
+    }
+    
+    
+
+}
