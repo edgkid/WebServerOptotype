a0288af1d85b8e561d18758693e41d63dd956001
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2018-03-27 15:59:25 -0430

    Registro Por Sintoma
    Agrego datos de sintomas primera parte, Registro por grupo de sintomas y mantengo su Id

--- DiagnosticDB.php
+++ DiagnosticDB.php
@@ -1,11 +1,7 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+require_once 'PgDataBase.php';
+require_once 'Diagnostic.php';
 /**
  * Description of DiagnosticDB
  *
@@ -14,12 +10,89 @@
 class DiagnosticDB {
     
     
-    public function proccessDataDiagnostic(){
+    public function proccessDataDiagnostic(array $obj){
         
         $responce = "listo para procesar datos";
+        
+        switch ($obj[0]->action){
+            
+            case '0':
+                //echo "salvar datos";
+                $this->saveDataDiagnostic($obj);
+                break;
+            
+            case '1':
+                echo "Consultar datos de interacción";
+                break;
+            
+            case '2':
+                echo "Actualizar datos de interacción";
+                break;
+            
+            case '3':
+                echo 'eliminar datos de interacion';
+                break;
+  
+        }
         
         return $responce;
     }
     
+    private function saveDataDiagnostic(array $obj){
+        
+        /*echo $obj[0]->idPatient.' ';
+        echo $obj[0]->yearsOld." ";
+        echo $obj[0]->gender." ";
+        echo $obj[0]->center." ";
+        echo $obj[0]->sustain." ";
+        echo $obj[0]->maintain." ";
+        echo $obj[0]->avRigth." ";
+        echo $obj[0]->avLeft." ";
+        echo $obj[0]->antecedentMon." ";
+        echo $obj[0]->antacedentDad." ";
+        echo $obj[0]->signalDefect." ";        
+        echo $obj[0]->typeTest." ";
+        echo $obj[0]->colaboratedGrade." ";*/
+        
+        $diagnostic = new Diagnostic();
+        $diagnostic->setIdPatient($obj[0]->idPatient);
+        
+        $this->saveDataDiagnosticSignalRegister($diagnostic, $obj);
+            
+    }
+    
+    private function saveDataDiagnosticSignalRegister (Diagnostic $diagnostic, array $obj){
+        
+        $fk = (int) $obj[0]->idPatient;
+        $connect = new PgDataBase();
+        $connect->getPgConnect();
+        $idTable = "idSignalRegister";
+        $query = " INSERT INTO Signal_BY_REGISTER (fk_idPatient) VALUES (".$fk."); ";
+        $query = $query." commit;";
+        $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+        
+        if ($result){
+            $query = " Select max (".$idTable.") from ";
+            $tableName = "Signal_BY_REGISTER";
+            $diagnostic->setIdSignalDefect($this->getAId($query, $tableName));
+        }
+            
+    }
+    
+    private function getAId ($query, $tableNanme){
+        $id = 0;
+        
+        $query = $query.$tableNanme;
+        
+        $result = pg_query($query);
+        
+        if ($row = pg_fetch_row($result)) {
+           
+            $id = $row[0]; 
+        }
+        
+        return $id;
+        
+    }
     
 }
