f0777b76a24a7335298c29fa5bde403714e348e2
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2018-03-31 18:44:09 -0430

    Api de defectos
    Se incluye clase Api de defecto para publicar su propio recurso

---
+++ ChromaticDefectApi.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'ChromaticDefectDB.php';
+/**
+ * Description of ChromaticDefect
+ *
+ * @author Edgar
+ */
+class ChromaticDefectApi {
+    
+    Public function API(){
+        
+        header('Content-Type: application/JSON');                
+        $method = $_SERVER['REQUEST_METHOD'];
+        switch ($method) {
+        case 'GET'://consulta
+            $this->getDefect();
+            break;     
+        case 'POST'://inserta
+            echo 'post';
+            break;                
+        case 'PUT'://actualiza
+            echo 'put';
+            break;      
+        case 'DELETE'://elimina
+            echo 'delete';
+            break;
+        default://metodo NO soportado
+            echo 'METODO NO SOPORTADO';
+            break;
+        }
+    }
+    
+    function getDefect(){
+       if ($_GET['action'] == 'antecedent'){
+           $db = new ChromaticDefectDB();
+           $response = $db->getChromaticDefect();
+           echo json_encode($response,JSON_PRETTY_PRINT);
+       }else{
+           $this->response(400);
+       }  
+    }
+    
+    function response($code=200, $status="", $message="") {
+       http_response_code($code);
+       if( !empty($status) && !empty($message) ){
+           $response = array("status" => $status ,"message"=>$message);  
+           echo json_encode($response,JSON_PRETTY_PRINT);    
+       }   
+    }  
+    
+    
+}
