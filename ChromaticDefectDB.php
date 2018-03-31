1425d7a3043648c499a3b238dff8b3cc396e1f09
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2018-03-31 18:44:30 -0430

    Defecto DB
    Clase para obtener datos de desfectos o antecedentes

---
+++ ChromaticDefectDB.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'PgDataBase.php';
+/**
+ * Description of ChromaticDefectDB
+ *
+ * @author Edgar
+ */
+class ChromaticDefectDB extends PgDataBase {
+    
+    public function getChromaticDefect (){
+        
+        $data = array();
+        $query = " SELECT idDefect, name FROM EYE_DEFECT";
+        $users = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+       
+        while ($line = pg_fetch_array($users, null, PGSQL_ASSOC)) {
+            $data []= array('idDefect'=>$line['iddefect'],'name'=>$line['name']);
+         }   
+
+        return $data; 
+    }
+    
+    
+}
